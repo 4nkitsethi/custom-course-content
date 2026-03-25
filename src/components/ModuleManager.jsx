25b814f12aacbe88321aa12a61be29de344e8677
Ankit Sethi <61274953+4nkitsethi@users.noreply.github.com>
2026-03-25 23:05:56 +0530

    Create ModuleManager component

---
+++ src/components/ModuleManager.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModuleManager = () => {
+    return (
+        <div>
+            <h2>Module Manager</h2>
+            {/* Module management logic goes here */}
+        </div>
+    );
+};
+
+export default ModuleManager;
